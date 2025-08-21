1b7954bbb6e4a13ae1173fc22cf13eeb3bf2cb35
liedsonlm7 <liedsonlm7@gmail.com>
2025-08-21 16:09:35 -0300

    Create prompt and services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import prompt from "prompt";
+import chalk from "chalk";
+
+import mainPrompt from "./prompts/prompt-main.js";
+import createQRCode from "./services/qr-code/create.js";
 
 async function main() {
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) await createQRCode();
+        if (choose.select == 2) console.log("escolheu o password");
+    });
 
+    prompt.start();
 }
 
 main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,9 +1,13 @@
+import chalk from "chalk";
+
 const mainPrompt = [
     {
         name: "select",
-        description: "Escolha a ferramenta ( 1- QRCODE ou ( 2-PASSWORD", 
+        description: chalk.yellow("Escolha a ferramenta ( 1- QRCODE ou ( 2-PASSWORD"), 
         pattern: /^[1-2]+$/, 
         message: "Escolha apenas entre 1 e 2", 
         required: true,
     }
 ];
+
+export default mainPrompt;
